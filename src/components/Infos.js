ba39a7fbb2d2981a420afb48bfb12188eb4f7813
ArianDervishaj <ArianDervishaj@icloud.com>
2022-11-26 18:30:41 +0100

    Finis les infos

--- src/components/Infos.js
+++ src/components/Infos.js
@@ -3,15 +3,15 @@ import photo from '../images/photo-de-moi.jpg';
 import emailLogo from '../images/email-logo.png';
 function Infos(){
     return (
-        <div>
-            <img src={photo} alt="moi " />
-            <h2>Arian Dervishaj</h2>
-            <h3>Étudiant</h3>
-            <h4><a href="https://ariandervishaj.ch/">ariandervishaj.ch</a></h4>
+        <div className='info'>
+            <img src={photo} alt="moi" className='info--photo' />
+            <h2 className='info--nom'>Arian Dervishaj</h2>
+            <h3 className='info--ocupation'>Étudiant</h3>
+            <h4 className='info--site'><a className='info--site' href="https://ariandervishaj.ch/">ariandervishaj.ch</a></h4>
             <a href="mailto:ariandervishaj@icloud.ch">
-                <button>
-                    <img src={emailLogo} alt="logo de mail" />
-                    Email
+                <button className='info--btn'>
+                    <img src={emailLogo} width='16px' alt="logo de mail" className='info--email-logo'/>
+                    <p className='info--mail-text'>Email</p>
                 </button></a>
 
         </div>
