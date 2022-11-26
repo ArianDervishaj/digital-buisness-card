7ecb5b680f1f7f5f39b984051fa6fbecd9864eb1
ArianDervishaj <ArianDervishaj@icloud.com>
2022-11-26 19:15:52 +0100

    fin du projet

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,14 @@
 import React from "react";
+import twitterLogo from '../images/twitter-logo.png';
+import githubLogo from '../images/github-logo.png';
 
 function Footer(){
     return (
-        <h1>Footer</h1>
+        <footer>
+            <a href='https://twitter.com/NuggetsLess'><img className="footer--logo" src={twitterLogo} alt="logo twitter"/></a>
+            <a href='https://github.com/ArianDervishaj'><img className="footer--logo" src={githubLogo} alt="logo twitter"/></a>
+        </footer>
+        
     )
 }
 
