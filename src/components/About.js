1893eca57b3f2836603ef6783bfc0304ca04d423
ArianDervishaj <ArianDervishaj@icloud.com>
2022-11-26 18:34:04 +0100

    contenu du a propos

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 function About(){
     return(
-        <h1>about</h1>
+        <div>
+            <h1>A propos</h1>
+            <p>
+            Étudiant en informatique, je suis passionné d'informatique depuis 
+            mon plus jeune âge. Lors de ma formation, j'ai eu la chance de pouvoir 
+            m'initier à plusieurs langages de programmation différents ainsi 
+            que de développer des projets non seulement amusants mais 
+            qui me poussait à me surpasser. Je suis actuellement 
+            en recherche de stage afin de compléter mon année et de 
+            continuer à développer mes capacités techniques.
+            </p>
+        </div>
+        
     )
 }
 
