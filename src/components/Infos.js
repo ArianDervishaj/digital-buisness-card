0e02ddd53a2df273514bda6c5ee8a8438911fa97
ArianDervishaj <ArianDervishaj@icloud.com>
2022-11-26 17:46:10 +0100

    toutes les infos du about

--- src/components/Infos.js
+++ src/components/Infos.js
@@ -1,8 +1,21 @@
 import React from 'react';
-
+import photo from '../images/photo-de-moi.jpg';
+import emailLogo from '../images/email-logo.png';
 function Infos(){
     return (
-        <h1>Infos</h1>
+        <div>
+            <img src={photo} alt="moi " />
+            <h2>Arian Dervishaj</h2>
+            <h3>Étudiant</h3>
+            <h4><a href="https://ariandervishaj.ch/">ariandervishaj.ch</a></h4>
+            <a href="mailto:ariandervishaj@icloud.ch">
+                <button>
+                    <img src={emailLogo} alt="logo de mail" />
+                    Email
+                </button></a>
+
+        </div>
+
     )
 }
 
